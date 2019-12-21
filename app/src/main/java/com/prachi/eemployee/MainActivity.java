485dffeac29feac8bcc7140082bc92076978a04a
prachiadhikari <prachiadhikari22@gmail.com>
2019-12-21 09:43:00 +0545

    edited

--- app/src/main/java/com/prachi/eemployee/MainActivity.java
+++ app/src/main/java/com/prachi/eemployee/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(MainActivity.this,ShowEmployeeActivity.class);
+                Intent intent =new Intent(MainActivity.this,SearchActivity.class);
                 startActivity(intent);
             }
         });
